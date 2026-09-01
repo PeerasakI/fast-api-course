d29b1e2f863cb2393df6e7894b3c4f3e1e14aaef
PeerasakIn <intarapaiboon@gmail.com>
2026-09-01 20:56:07 +0700

    modify app info

--- rent-a-room/main.py
+++ rent-a-room/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from rent-a-room!")
+from fastapi import FastAPI
 
+app = FastAPI(title= "Rent a Room API",
+              description="Book a stay in a room",
+              version= "1.0.0",
+              contact={"name":"Peerasak In",
+                       "email":"xxxx@gmail.com"})
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return {'message': "Welcome to my homepage!"}
